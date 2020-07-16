5ef6326e83ee9d9548c52e5143cd59f6a16d7687
Nitzan Ohana <this.vptr@gmail.com>
2020-07-16 17:10:31 +0300

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,10 +4,16 @@ module.exports = {
   "containers/container1/src/**/*.js": (absolutePaths) => {
     const cwd = process.cwd();
     const relativePaths = absolutePaths.map((file) =>
-      path.relative(cwd, file).replace("containers/container1/", "")
+      path.relative(cwd, file).replace("containers/container1/", "").join(" ")
     );
-    return `docker run --rm -v ${cwd}/containers/container1/src:/app/src container1 sh -c \"yarn lint  --fix ${relativePaths.join(
-      " "
-    )}\"`;
+    return `docker run --rm -v ${cwd}/containers/container1/src:/app/src container1 sh -c \"yarn lint  --fix ${relativePaths}\"`;
   },
 };
+
+// "containers/container2/src/**/*.py": (absolutePaths) => {
+//   const cwd = process.cwd();
+//   const relativePaths = absolutePaths.map((file) =>
+//     path.relative(cwd, file).replace("containers/container2/", "").join(" ")
+//   );
+//   return `docker run --rm -v ${cwd}/containers/container2/src:/app/src container1 sh -c \"pylint ${relativePaths}\"`;
+// },
