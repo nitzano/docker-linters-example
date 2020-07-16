87eedae22ae993d7d89276b0500fed8aae31e884
Nitzan Ohana <this.vptr@gmail.com>
2020-07-16 17:19:25 +0300

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,18 +3,21 @@ const path = require("path");
 module.exports = {
   "containers/container1/src/**/*.js": (absolutePaths) => {
     const cwd = process.cwd();
-    const relativePaths = absolutePaths.map((file) =>
-      path.relative(cwd, file).replace("containers/container1/", "").join(" ")
-    );
+    const relativePaths = absolutePaths
+      .map((file) =>
+        path.relative(cwd, file).replace("containers/container1/", "")
+      )
+      .join(" ");
     return `docker run --rm -v ${cwd}/containers/container1/src:/app/src container1 sh -c \"yarn lint  --fix ${relativePaths}\"`;
   },
   "containers/container2/src/**/*.py": (absolutePaths) => {
     const cwd = process.cwd();
     console.log(absolutePaths);
     const relativePaths = absolutePaths
-      .map((file) => path.relative(cwd, file))
+      .map((file) =>
+        path.relative(cwd, file).replace("containers/container2/", "")
+      )
       .join(" ");
-    return `echo ${cwd} ${relativePaths}`;
-    // return `docker run --rm -v ${cwd}/containers/container2/src:/app/src container2 sh -c \"pylint ${relativePaths}\"`;
+    return `docker run --rm -v ${cwd}/containers/container2/src:/app/src container2 sh -c \"pylint ${relativePaths}\"`;
   },
 };
