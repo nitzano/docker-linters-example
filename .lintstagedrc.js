b6e2a06ce86cada6140f8a748741ac982841aaf5
Nitzan Ohana <this.vptr@gmail.com>
2020-07-16 15:49:37 +0300

    works

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,6 +4,7 @@ module.exports = {
   'containers/container1/src/**/*.js': (absolutePaths) => {
     const cwd = process.cwd()
     const relativePaths = absolutePaths.map((file) => path.relative(cwd, file).slice(22))
-    return `docker run -w /app --rm -v ${cwd}/containers/container1/src:/app/src container1 sh -c \"yarn lint --quiet --fix ${relativePaths.join(' ')}\"`
+    console.log(relativePaths);
+    return `docker run -w /app --rm -v ${cwd}/containers/container1/src:/app/src container1 sh -c \"yarn lint  --fix ${relativePaths.join(' ')}\"`
   }
 }
